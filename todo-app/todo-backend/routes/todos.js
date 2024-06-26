5a090e3dd61d2664db3246899a10971d7c40208c
Jacob <jbale444@outlook.com>
2024-06-26 07:21:07 +0100

    Added logic for method to get a single to do from the database and began adding logic to update a todo via the REST api for exercise 12.7

--- todo-app/todo-backend/routes/todos.js
+++ todo-app/todo-backend/routes/todos.js
@@ -35,12 +35,22 @@ singleRouter.delete('/', async (req, res) => {
 
 /* GET todo. */
 singleRouter.get('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  const todo = {
+    text: req.todo.text,
+    done: req.todo.done
+  }
+  res.send(todo);
 });
 
 /* PUT todo. */
 singleRouter.put('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  const updatedTodo = {
+    text: req.body.text,
+    done: req.body.done
+  }
+  Todo.updateOne({_id:id}, {$set: updatedTodo})
+  console.log(req.body)
+  res.send(updatedTodo);
 });
 
 router.use('/:id', findByIdMiddleware, singleRouter)
